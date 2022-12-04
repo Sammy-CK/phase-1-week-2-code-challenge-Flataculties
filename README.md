98685d0aa4453f90e5a8f7a8ca7132a53cc88817
Sammy-CK <sammycherono2696@gmail.com>
2022-12-05 01:11:40 +0300

    Added styling

---
+++ README.md
@@ -0,0 +1,59 @@
+
+# Phase-1-week-2-code-challenge-Flataculties
+
+## Description
+Moringa School software development Flatiron phase-1-week-1-code-challenges involves building Flataculties, an app where you can vote for the cutest animal!. It covers three basic challenges solved using:
+HTML, a json server, JavaScript and css.
+
+1)  The first challenge involves seeing a list of all animal names by making a GET request.
+
+2)  The second challenge is for enabling clicking on an animal’s name to see its details i.e image and number of votes and should display the details of one animal at a time. 
+
+3)  The third challenge requires to be able to add the number of votes for each animal, then be displayed together with the animal’s details.
+
+
+## Installation
+
+To use this repo on your machine requires some simple steps
+
+### Alternative One
+
+- Open a terminal / command line interface on your computer
+- Clone the repo by using the following:
+
+        git@github.com:Sammy-CK/phase-1-week-2-code-challenge-Flataculties.git
+
+- Be patient as it creates a copy on your local machine for you.
+- Change directory to the repo folder:
+
+        cd phase-1-week-2-code-challenge-Flataculties
+
+Run this command to get the backend started:
+
+         json-server --watch db.json
+
+Test your server by visiting this route in the browser: 
+
+
+        http://localhost:3000/characters
+
+
+# Running the application
+
+To open site in browser while inside the cloned directory:
+
+
+        explorer.exe index.html
+
+Run program from the index.html file in browser.
+## Author
+
+The author of this program:
+
+- [@Sammy-CK](https://www.github.com/Sammy-CK)
+
+
+## License
+
+[ISC](https://choosealicense.com/licenses/isc/)
+
--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
 document.addEventListener('DOMContentLoaded',() => {
+
+    //assign ul where list will be appended
     const ul = document.getElementById('cuties');
+
+    //assign div container for animal details
     const imageAndVotes = document.getElementById('displayed');
 
-
+//fetch request to db.json server
     fetch('http://localhost:3000/characters')
     .then((response) => response.json())
     .then((data) => {
         data.forEach(animalData => {
 
+//create and assign new list for animal names
         let animal = document.createElement('li');
 
         animal.innerText =  animalData.name;
         ul.append(animal);
 
-
+//Click Eventlistener on an animal’s name to see its details i.e image and number of votes
     animal.addEventListener('click', () => {
-       let voteTotal = document.getElementById('animalVotes');
-        voteTotal.innerHTML = `<b>TOTAL VOTES:</b> ${animalData.votes}`;
 
-        animalImage.src = animalData.image
+        let voteTotal = document.getElementById('animalVotes');
+       let animalImage = document.getElementById('animalImage');
+       animalImage.src = animalData.image
 
-            imageAndVotes.append(animalImage);
-            imageAndVotes.append(voteTotal);
+// assign initial number of votes
+   voteTotal.innerHTML = `<b>TOTAL VOTES FOR ${animalData.name}:</b> ${animalData.votes}`;
 
 
-    let votaAdd = document.getElementById('addedVotes')
+    let voteAdd = document.getElementById('addedVotes');
 
     let addSubmit = document.getElementById('submitVotes');
 
-    addSubmit.addEventListener('click', () => {
-        animalData.votes = +animalData.votes + +votaAdd.value;
-        voteTotal.innerHTML = `<b>TOTAL VOTES:</b> ${animalData.votes}`;
+// assign new summed value of votes
+    let sumVotes = +animalData.votes
 
-                })
+    addSubmit.addEventListener('click', () => {
+        sumVotes = sumVotes + +voteAdd.value
+        voteTotal.innerHTML = `<b>TOTAL VOTES FOR ${animalData.name}:</b> ${sumVotes}`;
+    })
     
             })
        
